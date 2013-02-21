b3af25a85995e66fe2b3012c03bb08ab8022f045
Markus Scheidgen <markus.scheidgen@gmail.com>
2013-02-21 12:51:41 +0100

    Refactored all old implementations out of the code base. Organized all imports and removed warnings. Removed the old benchmarking test. All test and resources for the JDTAST model, all morsa and CDO stuff.

--- de.hub.emffrag.benchmarks/gen-src/de/hub/emffrag/bm/benchmark/util/ICandidateFactory.java
+++
@@ -1,9 +0,0 @@
-package de.hub.emffrag.bm.benchmark.util;
-
-import de.hub.emffrag.bm.benchmark.EMFFragCandidates;
-import de.hub.emffrag.bm.impl.AbstractImpl;
-
-public interface ICandidateFactory {
-
-    public AbstractImpl createCandidateImpl(EMFFragCandidates candiate);
-}
